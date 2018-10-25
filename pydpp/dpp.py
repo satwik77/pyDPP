298d24af12bace3029898b5b92725a83a4692ebd
satwik77 <satwik55@gmail.com>
2018-10-25 22:45:48 +0530

    Added support for sampling sentences

--- pydpp/dpp.py
+++ pydpp/dpp.py
@@ -41,11 +41,12 @@ class DPP():
     def compute_kernel(self, kernel_type='cos-sim', kernel_func=None, *args, **kwargs):
         if kernel_func == None:
             if kernel_type == 'cos-sim':
-                self.A = cosine_similary(self.X )
+                self.A = cosine_similarity(self.X )
             elif kernel_type == 'rbf':
                 self.A =rbf(self.X, **kwargs)
         else:
             self.A = kernel_func(self.X, **kwargs)
+
 
     def sample(self):
 
@@ -151,6 +152,150 @@ class DPP():
 
         return out
 
+class DPP_text():
+    """
+
+    Attributes
+    ----------
+    A : PSD/Symmetric Kernel
+
+
+    Usage:
+    ------
+
+    >>> from pydpp.dpp import DPP
+    >>> import numpy as np
+
+    >>> X = np.random.random((10,10))
+    >>> dpp = DPP(X)
+    >>> dpp.compute_kernel(kernel_type='rbf', sigma=0.4)
+    >>> samples = dpp.sample()
+    >>> ksamples = dpp.sample_k(5)
+
+
+    """
+
+
+
+    def __init__(self, X=None,  A=None, **kwargs):
+
+        self.X = X
+        if A:
+            self.A = A
+
+    def compute_kernel(self, kernel_type='cos-sim', kernel_func=None, *args, **kwargs):
+        if kernel_func == None:
+            if kernel_type == 'cos-sim':
+                self.A = sent_cosine_sim(self.X )
+            # elif kernel_type == 'rbf':
+            #     self.A =rbf(self.X, **kwargs)
+        else:
+            self.A = kernel_func(self.X, **kwargs)
+
+
+    def sample(self):
+
+        if not hasattr(self,'A'):
+            self.compute_kernel(kernel_type='cos-sim')
+
+        eigen_vals, eigen_vec = eig(self.A)
+        eigen_vals =np.real(eigen_vals)
+        eigen_vec =np.real(eigen_vec)
+        eigen_vec = eigen_vec.T
+        N = self.A.shape[0]
+        Z= list(range(N))
+
+        probs = eigen_vals/(eigen_vals+1)
+        jidx = np.array(np.random.rand(N)<=probs)    # set j in paper
+
+
+        V = eigen_vec[jidx]           # Set of vectors V in paper
+        num_v = len(V)
+
+
+        Y = []
+        while num_v>0:
+            Pr = np.sum(V**2, 0)/np.sum(V**2)
+            y_i=np.argmax(np.array(np.random.rand() <= np.cumsum(Pr), np.int32))
+
+            # pdb.set_trace()
+            Y.append(y_i)
+            V =V.T
+            ri = np.argmax(np.abs(V[y_i]) >0)
+            V_r = V[:,ri]
+
+
+            if num_v>0:
+                try:
+                    V = la.orth(V- np.outer(V_r, (V[y_i,:]/V_r[y_i]) ))
+                except:
+                    pdb.set_trace()
+
+            V= V.T
+
+            num_v-=1
+
+        Y.sort()
+
+        out = np.array(Y)
+
+        return out
+
+    def sample_k(self, k=5):
+
+        if not hasattr(self,'A'):
+            self.compute_kernel(kernel_type='cos-sim')
+
+        eigen_vals, eigen_vec = eig(self.A)
+        eigen_vals =np.real(eigen_vals)
+        eigen_vec =np.real(eigen_vec)
+        eigen_vec = eigen_vec.T
+        N =self.A.shape[0]
+        Z= list(range(N))
+
+        if k==-1:
+            probs = eigen_vals/(eigen_vals+1)
+            jidx = np.array(np.random.rand(N)<=probs)    # set j in paper
+
+        else:
+            jidx = sample_k_eigenvecs(eigen_vals, k)
+
+        V = eigen_vec[jidx]           # Set of vectors V in paper
+        num_v = len(V)
+
+        Y = []
+        while num_v>0:
+            Pr = np.sum(V**2, 0)/np.sum(V**2)
+            y_i=np.argmax(np.array(np.random.rand() <= np.cumsum(Pr), np.int32))
+
+            # pdb.set_trace()
+            Y.append(y_i)
+            # Z.remove(Z[y_i])
+            V =V.T
+            try:
+                ri = np.argmax(np.abs(V[y_i]) >0)
+            except:
+                print("Error: Check: Matrix PSD/Sym")
+                exit()
+            V_r = V[:,ri]
+            # nidx = list(range(ri)) + list(range(ri+1, len(V)))
+            # V = V[nidx]
+
+            if num_v>0:
+                try:
+                    V = la.orth(V- np.outer(V_r, (V[y_i,:]/V_r[y_i]) ))
+                except:
+                    print("Error in Orthogonalization: Check: Matrix PSD/Sym")
+                    pdb.set_trace()
+
+            V= V.T
+
+            num_v-=1
+
+        Y.sort()
+        out = np.array(Y)
+
+        return out
 
 
 
---
+++ pydpp/vec_kernels.py
@@ -0,0 +1,108 @@
+import difflib
+import pdb
+import gensim
+from time import time
+from scipy.spatial.distance import pdist, squareform
+import scipy
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
+
+embedding_path = '/scratchd/home/satwik/embeddings/'
+
+
+print('Loading Word2Vec')
+st_time = time()
+model = gensim.models.KeyedVectors.load_word2vec_format(embedding_path+'GoogleNews-vectors-negative300.bin.gz', binary=True)
+print('Word2vec Loaded')
+etime = (time() - st_time)/60.0
+print('Time Taken : {}'.format(etime))
+
+cos_sim = lambda a,b: dot(a, b)/(norm(a)*norm(b))
+rbf = lambda a,b, sigma : scipy.exp(-(np.sum( (a-b)**2 ) ** 2 )/ sigma ** 2)
+
+def sent2wvec(s):
+    v= []
+    for w in s:
+        try:
+            vec =  model[w]
+            v.append(vec)
+        except:
+            pass
+
+    v = np.array(v)
+    return v
+
+
+
+def sentence_compare(s1, s2, kernel='cos', **kwargs):
+    l1 = s1.split()
+    l2 = s2.split()
+    # pdb.set_trace()
+
+    v1= sent2wvec(l1)
+    v2= sent2wvec(l2)
+    # v2 = np.array([model.wv.word_vec(w) for w in l2])
+    score = 0
+    len_s1 = v1.shape[0]
+    # pdb.set_trace()
+    for v in v1:
+        if kernel == 'cos':
+            wscore = np.max(np.array([cos_sim(v,i) for i in v2] ))
+        elif kernel == 'rbf':
+            wscore = np.max(np.array([rbf(v,i, sigma) for i in v2] ))
+        else:
+            print('Error in kernel type')
+        score += wscore/len_s1
+
+    return score
+
+
+def sent_cosine_sim(X):
+    d=[]
+
+    for i in range(len(X) ):
+        td=[]
+        for j in range(len(X) ):
+            td.append(sentence_compare(X[i], X[j], 'cos'))
+        d.append(td)
+        # pdb.set_trace()
+    A= np.array(d)
+    print(A.shape)
+    V = (0.5*A)+ (0.5*A.T)
+
+    return V
+
+
+
+def sent_rbf(X, sigma=0.5):
+    d=[]
+
+    for i in range(len(X) ):
+        td=[]
+        for j in range(len(X) ):
+            td.append(sentence_compare(X[i], X[j], kernel='rbf', sigma=sigma))
+        d.append(td)
+        # pdb.set_trace()
+    A= np.array(d)
+    print(A.shape)
+    V = (0.5*A)+ (0.5*A.T)
+
+    return V
+
+if __name__ == '__main__':
+    sents =[]
+    sents.append('what is best way to make money online' )
+    sents.append('what should i do to make money online' )
+    sents.append('what should i do to earn money online' )
+    sents.append('what is the easiest way to make money online' )
+    sents.append('what is the easiest way to earn money online' )
+    sents.append('what s the easiest way to make money online' )
+    sents.append('what s the easiest way to earn money online' )
+    sents.append('what should i do to make money online online' )
+    sents.append('what is the best way to make money online' )
+    sents.append('what is the easiest way to make money online online' )
+
+    sent_cosine_sim(sents)
+
+
